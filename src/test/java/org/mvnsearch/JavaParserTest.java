9817fe95e05ff2a3495387264478a4e7854790bc
linux_china <libing.chen@gmail.com>
2020-04-18 13:01:09 -0700

    Java Parser test

---
+++ src/test/java/org/mvnsearch/JavaParserTest.java
@@ -0,0 +1,43 @@
+package org.mvnsearch;
+
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.printer.YamlPrinter;
+import org.junit.jupiter.api.Test;
+
+import static com.github.javaparser.StaticJavaParser.parse;
+
+/**
+ * Java Parser test
+ *
+ * @author linux_china
+ */
+public class JavaParserTest {
+    @Test
+    public void testParse() {
+        String code = "@interface AD { String foo(); }";
+        CompilationUnit cu = parse(code);
+        TypeDeclaration<?> typeDeclaration = cu.getTypes().get(0);
+        if (typeDeclaration instanceof ClassOrInterfaceDeclaration classOrInterfaceDeclaration) {
+            System.out.println(classOrInterfaceDeclaration.isInterface());
+        }
+        System.out.println(typeDeclaration.getName());
+    }
+
+    @Test
+    public void testGeneration() {
+        CompilationUnit cu = new CompilationUnit();
+        ClassOrInterfaceDeclaration myClass = cu.addClass("MyClass").setPublic(true);
+        myClass.addField(String.class, "name", Modifier.Keyword.PUBLIC);
+        System.out.println(myClass.toString());
+    }
+
+    @Test
+    void testYamlPrinter() {
+        YamlPrinter.print(StaticJavaParser.parse("class X{ private int id;}"));
+    }
+}

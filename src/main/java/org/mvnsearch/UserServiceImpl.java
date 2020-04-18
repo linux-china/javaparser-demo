652df33df284c9e688b868afc41ff7eebbcf03a2
linux_china <libing.chen@gmail.com>
2020-04-18 13:07:19 -0700

    User Service implementation

---
+++ src/main/java/org/mvnsearch/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package org.mvnsearch;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * User Service implementation
+ *
+ * @author linux_china
+ */
+public class UserServiceImpl implements UserService {
+    @Override
+    public @Nullable User findUserById(Integer id) {
+        return null;
+    }
+}
